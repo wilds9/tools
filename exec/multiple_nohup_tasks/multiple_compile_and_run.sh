1af39bc0bcd0ef41e05e83abf0e611bc278d522f
wilds9 <wilds9>
2020-07-15 12:42:34 +0800

    添加使用sh compile.sh编译代码的情形

--- exec/multiple_nohup_tasks/multiple_compile_and_run.sh
+++ exec/multiple_nohup_tasks/multiple_compile_and_run.sh
@@ -17,5 +17,7 @@ do
     # 用icc命令编译实在太原始
     # 代码、编译文件、运行出的数据在同一级目录下也是不理想的
     # 推荐使用cmake进行管理
+
     cd $i && icc -o ${PREFIX}${i}.out *.cpp && nohup.sh ${PREFIX}${i}.out && cd ..
+    # cd $i && sh compile.sh && mv a.out ${PREFIX}${i}.out && nohup.sh ${PREFIX}${i}.out && cd ..
 done
